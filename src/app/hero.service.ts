1a4f6c38e3911e3c930a91ce9482f3a0b0387a42
florian pitelet <pitchupdev@outlook.com>
2022-03-02 17:12:04 +0100

    debut css (navbar ok )

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Hero } from './hero';
 import { environment } from 'src/environments/environment';
+import { MessageService } from './message.service';
 
 
 @Injectable({
@@ -13,24 +14,30 @@ export class HeroService{
 
     private apiServerUrl = environment.apiUrl;
 
-    constructor(private http: HttpClient){
+    constructor(private http: HttpClient, private messageService: MessageService){
 
     }
+
     public getHero(heroId: number): Observable<Hero>{
+        this.messageService.add(`Heros récupéré`);
         return this.http.get<Hero>(`${this.apiServerUrl}/hero/find/${heroId}`);
     }
 
     public getHeroes(): Observable<Hero[]>{
+        this.messageService.add(`Heros récupérés`);
         return this.http.get<Hero[]>(`${this.apiServerUrl}/hero/all`);
     }
     public addHero(hero: Hero): Observable<Hero>{
+        this.messageService.add(`Heros ajouté`);
         return this.http.post<Hero>(`${this.apiServerUrl}/hero/add`, hero);
     }
     public updateHero(hero: Hero): Observable<Hero>{
+        this.messageService.add(`Heros mis à jour`);
         return this.http.put<Hero>(`${this.apiServerUrl}/hero/update`, hero);
         
     }
     public deleteHero(heroId: number): Observable<void>{
+        this.messageService.add(`Heros supprimé`);
         return this.http.delete<void>(`${this.apiServerUrl}/hero/delete/${heroId}`);
     }
 }
